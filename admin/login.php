0cc556895d541b23aadd8ad02563e3c14db8c187
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-10 19:18:16 +0530

    Login Page

---
+++ admin/login.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buy a Business | Sell a Business | BizQuest - The Original Business for Sale Website</title>
+
+   <link rel="stylesheet" href="./asset/css/style.css">
+    
+<link type='text/css' rel='stylesheet' href='./asset/css/welcome.css' />
+</head>
+<style>
+    .btn-hidden {
+        color: transparent !important;
+        background-color: transparent !important;
+        border-color: transparent !important;
+        box-shadow: none !important;
+        pointer-events: none;
+        cursor: default;
+    }
+
+    .typeahead.dropdown-menu:before {
+        left: 21px;
+        right: inherit;
+        max-width: 10px;
+    }
+    .typeahead.dropdown-menu:after {
+        left: 21px;
+        right: inherit;
+        max-width: 10px;
+    }
+
+    .btn.btn-sell-b.pull-right.hidden-xs {
+        background: #39424c !important;
+        border: 1px solid #FFFFFF !important;
+     }
+    .btn.btn-sell-b.pull-right.hidden-xs:hover {
+            border: 1px solid #aed6f8 !important;
+            color: #aed6f8 !important;
+        }
+</style>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 visible-md visible-lg" style="padding-top: 10px">
+                &nbsp;
+            </div>
+        </div>
+        <div class="row">
+            <form action="/login/dologin/" method="POST" id="loginForm">
+                <input name="__RequestVerificationToken" type="hidden" value="8aJTSm3mJJWLAI3o9WH9BRRwqbGOd8g7fkZxcust8U3suDFISVIMBIZxwNjLekq0XXVvyLCLtuiNzSIL3H7Kznb3IhesH5AOLan992IOHQ01" />
+                <input type="hidden" name="ReturnUrl" />
+                <div class="col-md-8" style="padding-top: 20px">
+                    <div class="loginbox">
+                        <div class="loginform">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2 style="margin:5px 0 15px 0">Please Login</h2>
+                                    <p style="color:#7d7d7d; margin-right:10px; margin-bottom:20px">Log in with your username or email address and password below:</p>
+                                    <div class="alert alert-danger errorMessage" style="display: none"></div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="form-group login-id">
+                                        <label for="email">Username or Email Address</label>
+                                        <input type="text" class="form-control" id="email" name="email" value="pratapsinghatul111@gmail.com">
+                                    </div>
+                                    <div class="form-group current-pw">
+                                        <label for="password">Password</label>&nbsp;(case sensitive)
+                                        <input type="password" class="form-control" id="password" name="password">
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" checked="checked" name="RememberMe"> Save my login information on this computer
+                                        </label>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary" aria-describedby="helpBlock" style="margin-top:10px">Sign In</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><div class="login-grey">
+                            <strong><a href="https://www.bizquest.com/login/forgot/">Forgot your password?</a></strong><strong style="float:right;"><a href="https://www.bizquest.com/Login/Register/">&raquo;&nbsp;Click here to register as a new user</a></strong>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    
+    
+    <div class="truste">
+        <p><strong>Our site is secure.</strong><br>Your information is sent to us via a secure, encrypted connection to ensure your private information is kept safe. </p>
+    </div>
+    <p><strong>Need Assistance?</strong></p>
+    <ul class="list-unstyled">
+        <li><i class="fa fa-phone"></i> <a href="tel:+1-888-280-3815">888-280-3815</a></li>
+        <li><i class="fa fa-clock-o"></i> Mon - Fri, 9am - 5pm Pacific</li>
+        <li><i class="fa fa-info-circle"></i> <a href="https://www.bizquest.com/Support/Contact.aspx">Contact Us</a></li>
+    </ul>
+    
+    <br id="moveMeBottom">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
