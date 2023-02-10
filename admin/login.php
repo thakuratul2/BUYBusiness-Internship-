d6d64e5a42de66502d2d5d45aa49fa0d237a0354
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-10 20:14:53 +0530

    User Page

--- admin/login.php
+++ admin/login.php
@@ -7,8 +7,10 @@
     <title>Buy a Business | Sell a Business | BizQuest - The Original Business for Sale Website</title>
 
    <link rel="stylesheet" href="./asset/css/style.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     
-<link type='text/css' rel='stylesheet' href='./asset/css/welcome.css' />
+<link type='text/css' rel='stylesheet' href='./asset/css/welcome.css'/>
 </head>
 <style>
     .btn-hidden {
@@ -41,14 +43,155 @@
         }
 </style>
 <body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.html">BizQuest</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
     <div class="container">
+        <style type="text/css">
+    .loginbox {
+        -webkit-box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+        -moz-box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+        box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+        border-radius: 5px;
+        margin-bottom: 60px;
+    }
+
+    .loginform {
+        padding: 20px;
+    }
+
+    .login-grey {
+        background-color: #eee;
+        padding: 10px 20px 15px 20px;
+    }
+
+    .checkbox {
+        margin-top: -10px !important;
+    }
+
+    .help-alert {
+        margin-top: 5px;
+        color: #CE4844;
+    }
+
+    /*.glyphicon {
+        padding-right: 5px;
+    }*/
+
+    .list-unstyled i {
+        width: 20px;
+    }
+
+    .tooltip {
+        top: 169px !important;
+        left: 112px !important;
+    }
+
+    a[href^=tel], a[href^=tel]:hover {
+        color: #333;
+        text-decoration: none;
+    }
+
+    .BQfooter a {
+        color: #333;
+    }
+
+
+    @media (min-width: 768px) {
+        .truste {
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        .mobile-only, .mobile-only:hover {
+            text-decoration: none;
+            color: #333;
+        }
+    }
+
+    @media ( max-width: 992px) {
+        .loginbox {
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+
+        .list-unstyled > li {
+            display: inline-block;
+            padding-right: 25px;
+            font-size: 120%;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .btn-primary {
+            width: 100%;
+        }
+
+        .loginbox {
+            margin-bottom: 5px;
+            background-color: #f5f5f5;
+        }
+
+        .login-grey {
+            background-color: #ddd;
+        }
+
+        .truste {
+            margin: 20px 0;
+        }
+
+            .truste p {
+                font-size: 115%;
+                line-height: 1.6em;
+            }
+
+        .list-unstyled > li {
+            font-size: 120%;
+        }
+
+        a[href^=tel] {
+            color: #428bca;
+            text-decoration: none;
+        }
+    }
+</style>
         <div class="row">
             <div class="col-md-12 visible-md visible-lg" style="padding-top: 10px">
                 &nbsp;
             </div>
         </div>
         <div class="row">
-            <form action="/login/dologin/" method="POST" id="loginForm">
+            <form action="" method="POST" id="loginForm">
                 <input name="__RequestVerificationToken" type="hidden" value="8aJTSm3mJJWLAI3o9WH9BRRwqbGOd8g7fkZxcust8U3suDFISVIMBIZxwNjLekq0XXVvyLCLtuiNzSIL3H7Kznb3IhesH5AOLan992IOHQ01" />
                 <input type="hidden" name="ReturnUrl" />
                 <div class="col-md-8" style="padding-top: 20px">
@@ -82,7 +225,7 @@
                                 </div>
                             </div>
                         </div><div class="login-grey">
-                            <strong><a href="https://www.bizquest.com/login/forgot/">Forgot your password?</a></strong><strong style="float:right;"><a href="https://www.bizquest.com/Login/Register/">&raquo;&nbsp;Click here to register as a new user</a></strong>
+                            <strong><a href="">Forgot your password?</a></strong><strong style="float:right;"><a href="register.php">&raquo;&nbsp;Click here to register as a new user</a></strong>
                         </div>
                     </div>
                 </div>
@@ -96,7 +239,7 @@
     <ul class="list-unstyled">
         <li><i class="fa fa-phone"></i> <a href="tel:+1-888-280-3815">888-280-3815</a></li>
         <li><i class="fa fa-clock-o"></i> Mon - Fri, 9am - 5pm Pacific</li>
-        <li><i class="fa fa-info-circle"></i> <a href="https://www.bizquest.com/Support/Contact.aspx">Contact Us</a></li>
+        <li><i class="fa fa-info-circle"></i> <a href="">Contact Us</a></li>
     </ul>
     
     <br id="moveMeBottom">
@@ -104,5 +247,14 @@
             </form>
         </div>
     </div>
+    <script type="text/javascript" defer>
+            $(function () {
+                if (window._beacons && window._beacons.length > 0) {
+                    $(this).sendBeacons();
+                }
+            });
+        </script>
+        <script src="./js/main.js"></script>
 </body>
+
 </html>
--- admin/register.php
+++ admin/register.php
@@ -1,36 +1,356 @@
-<!doctype html>
-<html lang="en">
 
+
+<!DOCTYPE html>
+<html lang="en-US">
 <head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <meta charset="utf-8" />
+        <title>Buy a Business | Sell a Business | BizQuest - The Original Business for Sale Website</title>
+            <link rel="canonical" href="https://www.bizquest.com/Login/Register/" />
+    <link type='text/css' rel='stylesheet' href='./asset/css/style.css' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/fontawesome.min.css">
+    <link type='text/css' rel='stylesheet' href='./asset/css/welcome.css' />
 
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style type="text/css">
+        .loginbox h2 {
+            margin-top: 10px;
+        }
 
+        #loginForm h3 {
+            margin-top: 50px;
+        }
+
+        .loginbox .buyerNwsltr {
+            margin-top: 5px !important;
+        }
+
+        .registerBtn {
+            margin-right: 15px;
+        }
+
+        .registerConsent {
+            line-height: 18px;
+            padding-top: 6px;
+            margin-right: 30px;
+        }
+
+        ul.registerBenefits {
+            font-size: 19px;
+            margin-top: 20px;
+            margin-bottom: 40px;
+        }
+
+        ul.registerBenefits li {
+            margin-bottom: 7px;
+        }
+
+        ul.registerBenefits li:before {
+            font-family: 'FontAwesome';
+            content: '\f067';
+            margin-right: 10px;
+            color: #2EB83A;
+        }
+    </style>
+    <style type="text/css">
+        .loginbox {
+          -webkit-box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+          -moz-box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+          box-shadow: 2px 2px 15px 0px rgba(125,125,125,0.7);
+          border-radius: 5px;
+          margin-bottom: 60px;
+        }
+
+        .loginform {
+          padding: 15px 20px;
+        }
+
+        .login-grey {
+          background-color: #eee;
+          padding: 10px 20px 10px 20px;
+          border-bottom-left-radius: 5px;
+          border-bottom-right-radius: 5px;
+        }
+
+        .checkbox {
+            margin-top: -10px !important;
+        }
+
+        .help-alert {
+            margin-top: 5px;
+            color: #CE4844;
+        }
+
+        .list-unstyled i {
+            width: 20px;
+        }
+
+        .tooltip {
+            top: 169px !important;
+            left: 112px !important;
+        }
+
+        a[href^=tel], a[href^=tel]:hover {
+            color: #333;
+            text-decoration: none;
+        }
+
+        .BQfooter a {
+            color: #333;
+        }
+
+
+        @media (min-width: 768px) {
+            .truste {
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+
+            .mobile-only, .mobile-only:hover {
+                text-decoration: none;
+                color: #333;
+            }
+        }
+
+        @media ( max-width: 992px) {
+            .loginbox {
+                margin-top: 10px;
+                margin-bottom: 20px;
+            }
+
+            .list-unstyled > li {
+                display: inline-block;
+                padding-right: 25px;
+                font-size: 120%;
+            }
+        }
+
+        @media (max-width: 768px) {
+            .btn-primary {
+                width: 100%;
+            }
+
+            .loginbox {
+                margin-bottom: 5px;
+                background-color: #f5f5f5;
+            }
+
+            .login-grey {
+                background-color: #ddd;
+            }
+
+            .truste {
+                margin: 20px 0;
+            }
+
+                .truste p {
+                    font-size: 115%;
+                    line-height: 1.6em;
+                }
+
+            .list-unstyled > li {
+                font-size: 120%;
+            }
+
+            a[href^=tel] {
+                color: #428bca;
+                text-decoration: none;
+            }
+        }
+    </style>
+
+    
+    
+    <script type="text/javascript">
+        var _$_callbacks = [];
+        var $ = $ || function (onload) {
+            _$_callbacks.push(onload);
+        };
+    </script>
 </head>
-
 <body>
-  <header>
-    <!-- place navbar here -->
-  </header>
-  <main>
+<noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-MK7XDN8' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MK7XDN8');</script>
+    
+<style>
+    .btn-hidden {
+        color: transparent !important;
+        background-color: transparent !important;
+        border-color: transparent !important;
+        box-shadow: none !important;
+        pointer-events: none;
+        cursor: default;
+    }
 
-  </main>
-  <footer>
-    <!-- place footer here -->
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    .typeahead.dropdown-menu:before {
+        left: 21px;
+        right: inherit;
+        max-width: 10px;
+    }
+    .typeahead.dropdown-menu:after {
+        left: 21px;
+        right: inherit;
+        max-width: 10px;
+    }
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
+    .btn.btn-sell-b.pull-right.hidden-xs {
+        background: #39424c !important;
+        border: 1px solid #FFFFFF !important;
+     }
+    .btn.btn-sell-b.pull-right.hidden-xs:hover {
+            border: 1px solid #aed6f8 !important;
+            color: #aed6f8 !important;
+        }
+</style>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.html">BizQuest</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+    
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 visible-md visible-lg" style="padding-top: 10px">
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        <form action="/Login/DoRegister/" method="POST" id="loginForm">
+            <input name="__RequestVerificationToken" type="hidden" value="4gEGjZsoatWge7qF1UCjZVcIrorWQevviWY4Ob7swt4wL2j-_Jg44Lt5fxq8kKlnx43OQhawT_OApDeGaoDggwn-0yGqKn8omiVDmiX3mEo1" />
+            <input type="hidden" name="ReturnUrl" />
+            <input type="hidden" name="LoginEnabled" value="true" />
+            <input type="hidden" name="UserType" value="Buyer" />
+            <div class="col-md-6" style="padding-top: 20px">
+                <div class="loginbox">
+                    <div class="loginform">
+                        <h2>Create a BizQuest Account</h2>
+                        <p>Registration is FREE and easy, and requires no personal information other than your email address.</p>
+                        <div class="alert alert-danger errorMessage" style="display: none"></div>
+
+                        <div class="form-group register-id">
+                            <label for="email">Your Email Address</label>
+                            <input type="text" class="form-control gdpr-ca-email" id="email" name="email" value="">
+                        </div>
+                        <div class="form-group new-pw">
+                            <label for="password">Password</label> (case sensitive)
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+                        <div class="form-group confirm-pw">
+                            <label for="ConfirmPassword">Confirm Password</label>
+                            <input type="password" class="form-control" id="ConfirmPassword" name="ConfirmPassword">
+                        </div>
+                        <div class="checkbox buyerNwsltr">
+                            <label><input class="gdpr-ca-clickable" type="checkbox" checked='checked' name="BuyerNewsletter" value="true"> Send me the BizQuest Buyer's Newsletter. </label>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-large pull-left registerBtn" aria-describedby="helpBlock">Sign Up</button>
+                            <p class="registerConsent"><small>By clicking <b>Sign Up</b>, I agree to abide by the BizQuest <a href="/terms/">Terms of Use</a> and <a href="/PrivacyPolicy/">Privacy Notice</a>.</small></p>
+                        </div>
+
+
+                    </div>
+                    <div class="login-grey">
+                        <strong><a href="login.php">Already a member?</a></strong>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h3>Sign Up to get these benefits!</h3>
+                <ul class="list-unstyled registerBenefits">
+                    <li>Keep track of businesses you have inquired about</li>
+                    <li>Save Listings you want to remember</li>
+                    <li>Save Searches for faster results</li>
+                    <li>Create customized Email Search</li>
+                    <li>Receive <em>Agents</em> alerting you of new listings</li>
+                </ul>
+
+                <div class="truste">
+                    <p><strong>Our site is secure.</strong><br>Your information is sent to us via a secure, encrypted connection to ensure your private information is kept safe. </p>
+                </div>
+                <p><strong>Need Assistance?</strong></p>
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-phone"></i> <a href="tel:+1-888-280-3815">888-280-3815</a></li>
+                    <li><i class="fa fa-clock-o"></i> Mon - Fri, 9am - 5pm Pacific</li>
+                    <li><i class="fa fa-info-circle"></i> <a href="">Contact Us</a></li>
+                </ul>
+
+                <br id="moveMeBottom">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+    <script src=".js/main.js"/>
+
+
+    <script type="text/javascript" defer>
+        $(function () {
+            var SingleSignOn = {
+                OpenPasswordForgot: function () {
+                    window.location = '/Login/Forgot/';
+                }
+            };
+
+            var form = $('#loginForm').form();
+            var eml = form.email('.register-id', 'Enter a valid Email Address.');
+
+            var pw = form.group();
+            pw.length('.new-pw', 6, 50, 'Passwords must be at least six characters.');
+            pw.html('.new-pw', 'Passwords cannot contain HTML code.');
+
+            var confirm = form.group();
+            confirm.require('.confirm-pw', 'Confirm your password again.');
+            confirm.match('.confirm-pw', '.new-pw', 'Password and Confirm Password do not match.');
+
+            $('#loginForm').submit(function (e) {
+                e.preventDefault();
+                if (form.isValid()) {
+                    $.ajax({
+                        method: 'POST',
+                        url: '/Login/DoRegister/',
+                        data: $('#loginForm').serialize(),
+                        success: function (res) {
+                            if (res.Error) {
+                                $('.errorMessage').html(res.Error).show();
+                            } else if (res.Redirect) {
+                                window.location = res.Redirect;
+                            }
+                        }
+                    });
+                }
+                return false;
+            });
+        });
+    </script>
 
 </html>
